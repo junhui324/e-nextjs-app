1628e5747f74341c25b844ad5fddacfdb4a97f42
junhui324 <122953242+junhui324@users.noreply.github.com>
2023-05-23 14:39:52 +0900

    first commit

---
+++ pages/read/[id].tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '@/components/Layout';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WEB!</title>
+      </Head>
+      <h2>Read</h2>
+      Hello, Read!
+    </>
+  );
+}
